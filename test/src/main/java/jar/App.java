ddbe898cdfaa01578617606c9df5bbe34a77a57e
Rayane BEN-HMIDANE <rayane.benhmidane32@hotmail.fr>
2020-10-06 17:25:39 +0200

    Fix Travis

--- test/src/main/java/jar/App.java
+++ test/src/main/java/jar/App.java
@@ -8,6 +8,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		System.out.println( "Hello World!" );
+		
+		try {
+			Thread.sleep(1000*3600);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println( "END OF PROGRAM!" );
     }
 }
