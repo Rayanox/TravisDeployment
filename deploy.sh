6fb3b937c043cf7005a02b3413aa18dba0cd485d
Rayane BEN-HMIDANE <rayane.benhmidane32@hotmail.fr>
2020-10-07 11:23:57 +0200

    Test Travis - Refacto

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+jarName=$1
+PATH_LIVRAISON_CIBLE=$2
+
+echo "jarName = $jarName"
+echo "PATH_LIVRAISON_CIBLE = $PATH_LIVRAISON_CIBLE"
+qsdf
+ls
+
+# processStarted=`ps aux | grep "$jarName" | awk '{if($11 != "grep") {print $2} }'` 
+# if [ -z "$processStarted" ] ; then echo "Process non execute" ; else echo "Process deja en cours d'execution, kill maintenant..." && kill $processStarted ; fi
+# nohup java -jar $PATH_LIVRAISON_CIBLE/$jarName > $PATH_LIVRAISON_CIBLE/logStart.txt &
